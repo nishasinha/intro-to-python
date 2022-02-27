c6ab7d5009c70537c9ef074e223f5c34dcd92e99
Nisha Kumari <nisha.n.kumari@oracle.com>
2022-02-27 22:43:38 +0530

    Python install and new project

---
+++ src/python/01.py
@@ -0,0 +1,27 @@
+"""
+# Python
+
+1. Install python in system.
+2. python --version
+3. packages are installed via 'pip'
+`python3 -m pip install pandas`
+
+
+# New project Creation
+1. Create project directory.
+2. Create virtual env specific to project. Use venv module.
+`python3 -m venv venv`
+3. Activate virtual env.
+`source venv/bin/activate`
+4. Use python terminal or run python files.
+`python`
+`python main.py`
+4. Install libraries with specific version as needed.
+`python3 -m pip install pandas`
+5. Capture all libraries for project.
+`python3 -m pip list`
+`python3 -m pip freeze`
+`python3 -m pip freeze > requirements.txt`
+6. Use requirements.txt
+`python3 -m pip install -r requirements.txt`
+"""
